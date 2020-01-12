179c43b403d077ca87a551f4b99844cffc7d038e
eliyahu2323 <eliavraham1919@gmail.com>
2020-01-12 13:18:24 +0200

    finish fruit and robot class

--- src/gameClient/Fruits.java
+++ src/gameClient/Fruits.java
@@ -2,39 +2,54 @@ package gameClient;
 
 import java.util.ArrayList;
 
+import Server.fruits;
 import utils.Point3D;
 
 
 public class Fruits {
 
 	private double value;
-	private int type;
+	private enum type {banana , apple};
 	private Point3D pos;
+	private type fruit; 
 	
-	
-	
+	public Fruits(double v,Point3D p,int t) {
+		this.value=v;
+		this.pos=p;
+		if(t==1) {
+			fruit=type.apple;
+		}
+		else if(t==-1) {
+			fruit=type.banana;
+		}
+		else {
+			fruit=null;
+		}
+
+	}
+
 	public double getValue() {
 		return this.value;
 	}
-	
-	public int getType() {
-		return this.type;
+
+	public type getType() {
+		return this.fruit;
 	}
-	
+
 	public Point3D getPos() {
 		return this.pos;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
