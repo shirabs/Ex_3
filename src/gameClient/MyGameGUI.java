9f008e7d78b3ece2fca9628dc48b066b5bec9181
shirabs <shira159159@gmail.com>
2020-01-20 16:02:50 +0200

    EX_3 done1

--- src/gameClient/MyGameGUI.java
+++ src/gameClient/MyGameGUI.java
@@ -402,7 +402,7 @@ public class MyGameGUI {
 			double spdt=ag.shortestPathDist(src, a);
 			if(spd>spdt||sp.size()==1){
 				sp=spt;
-				spd=sptd;
+				spd=spdt;
 			}
 		}
 		return sp.get(1).getKey();
